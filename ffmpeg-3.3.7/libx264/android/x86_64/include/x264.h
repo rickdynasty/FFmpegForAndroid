ba9b7656fcf7c0b5d5df790fb09524f6de7c0c04
rick_tan <rick_tan@qq.com>
2018-05-22 12:24:00 +0800

    build ffmpeg3.3.7 sucess,upload build so

--- ffmpeg-3.3.7/libx264/android/x86_64/include/x264.h
+++ ffmpeg-3.3.7/libx264/android/x86_64/include/x264.h
@@ -43,13 +43,7 @@ extern "C" {
 
 #include <stdarg.h>
 
-//#include "x264_config.h"
-#define X264_BIT_DEPTH     8
-#define X264_GPL           1
-#define X264_INTERLACED    1
-#define X264_CHROMA_FORMAT 0
-#define X264_VERSION ""
-#define X264_POINTVER "0.148.x"
+#include "x264_config.h"
 
 #define X264_BUILD 148
 
